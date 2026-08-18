4e016db675df8b52bc73ad777514df71fd4a8772
tiendat13ns <dat96133@gmail.com>
2026-08-18 20:48:46 +0700

    chore: allow Vercel demo domain in backend CORS
    Frontend now deployed at https://tcga-github.vercel.app, backend on Render.

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,7 @@ logging.basicConfig(level=logging.INFO)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:1302"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:1302", "https://tcga-github.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
